7c3212178fa6ebeb54fbaffdf4cbcde78b5ab641
powermano <894773140@qq.com>
2017-08-01 15:55:15 +0800

    fileread

--- fileread.py
+++ fileread.py
@@ -1,4 +1,24 @@
-file=open('E:/test1.txt','w+')
-file.write('hello \n')
-file.write('add \n')
-file.close()
+import os
+from os import listdir, getcwd
+from os.path import join
+import  numpy as np
+thresh = 0.24
+valpath = 'E:/valdata/vallabels/'
+finalpath = 'E:/valdata/alllabels.txt'
+all_obj=open(finalpath,'w')
+val_list = os.listdir(valpath)
+for val_obj in val_list:
+    val_path = os.path.join(valpath,val_obj)
+    text=open(val_path,'r').read().strip().split()
+    for x in range(len(text)):
+        all_obj.write(text[x]+' ')
+    all_obj.write('\n')
+all_obj.close()
+
+
+
+
+
+
+
+
