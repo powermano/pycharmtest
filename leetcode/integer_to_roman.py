31ba8d2ffae23de61a4817754781b7ede9121ba2
powermano <894773140@qq.com>
2017-10-16 19:57:43 +0800

    add problem: integer to roman

---
+++ leetcode/integer_to_roman.py
@@ -0,0 +1,33 @@
+def intToRoman(self, num):
+    values = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ]
+    numerals = [ "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" ]
+    res = ""
+    for i, v in enumerate(values):
+        res += (num//v) * numerals[i]
+        num %= v
+    return res
+
+
+
+
+from enum import Enum
+
+Month = Enum('Month',('Jan','Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
+
+for name,member in Month.__members__.items():
+    # value属性则是自动赋给成员的int常量，默认从1开始计数
+    print(name,'=>',member,',',member.value)
+
+# 输出结果
+# Jan => Month.Jan , 1
+# Feb => Month.Feb , 2
+# Mar => Month.Mar , 3
+# Apr => Month.Apr , 4
+# May => Month.May , 5
+# Jun => Month.Jun , 6
+# Jul => Month.Jul , 7
+# Aug => Month.Aug , 8
+# Sep => Month.Sep , 9
+# Oct => Month.Oct , 10
+# Nov => Month.Nov , 11
+# Dec => Month.Dec , 12
