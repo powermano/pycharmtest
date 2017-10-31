73df688a8469dd03227d20d562e6971190544c54
powermano <894773140@qq.com>
2017-10-31 16:06:49 +0800

    add problem : isValid

--- leetcode/isValid.py
+++ leetcode/isValid.py
@@ -4,9 +4,18 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        if len(s) % 2 == 1:
-            return False
-        p = [[False]*len(s) for i in range(len(s))]
-        for i in range(len(s)):
-            for j in range(i+1,len(s)):
-                pass
+        # if len(s) % 2 == 1:
+        #     return False
+        # p = [[False]*len(s) for i in range(len(s))]
+        # for i in range(len(s)):
+        #     for j in range(i+1,len(s)):
+        #         pass
+        res = []
+        dict = {'(': ')', '{': '}', '[': ']'}
+        for i in s:
+            if i in dict:
+                res.append(i)
+            elif i in dict.values():
+                if res == [] or dict[res.pop()] != i:
+                    return False
+        return res == []
