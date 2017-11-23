5d07817362965de065d38144d3a21108a0109ae8
powermano <894773140@qq.com>
2017-11-23 11:06:46 +0800

    add xml test

--- xml_test.py
+++ xml_test.py
@@ -9,10 +9,10 @@ def parse_rec(filename):
   objects = []
   for obj in tree.findall('object'):
     obj_struct = {}
-    # obj_struct['name'] = obj.find('name').text
-    # obj_struct['pose'] = obj.find('pose').text
-    # obj_struct['truncated'] = int(obj.find('truncated').text)
-    # obj_struct['difficult'] = int(obj.find('difficult').text)
+    obj_struct['name'] = obj.find('name').text
+    obj_struct['pose'] = obj.find('pose').text
+    obj_struct['truncated'] = int(obj.find('truncated').text)
+    obj_struct['difficult'] = int(obj.find('difficult').text)
     bbox = obj.find('bndbox')
     obj_struct['bbox'] = [int(bbox.find('xmin').text),
                           int(bbox.find('ymin').text),
